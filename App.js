eeffefa5def85d5651d2fe23d436a6df0d83d894
binhui <teopinghwee@gmail.com>
2021-05-21 19:03:14 +0800

    final

--- App.js
+++ App.js
@@ -1,18 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
 
 export default function App() {
   const [loading, setLoading] = useState(true);
+  const [arrival, setArrival] = useState("");
+  const [nextArrival, setNextArrival] = useState("");
+  const [duration, setDuration] = useState("");
+  const [nextDuration, setNextDuration] = useState("");
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
+  const [busNumber, setBusNumber] = useState("155")
+  const time = new Date(arrival);
+  const nextTime = new Date(nextArrival);
+
+  function loadBusStopData() {
+    setLoading(true);
+
+    fetch(BUSSTOP_URL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const myBus = responseData.services.filter(
+          (item) => item.no === busNumber
+        )[0];
+        
+        setArrival(myBus.next.time);
+        setNextArrival(myBus.subsequent.time);
+        setDuration(myBus.next.duration_ms)
+        setNextDuration(myBus.subsequent.duration_ms)
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    const interval = setInterval(loadBusStopData, 10000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bus arrival time</Text>
       <Text style={styles.arrivalTime}>
-        {loading ? <ActivityIndicator size="large" /> : "Loaded"}
+        {loading ? <ActivityIndicator size="large" /> : `${time.toLocaleTimeString()} (${duration/1000} sec)`}
+      </Text>
+      <Text style={styles.nextArrivalTime}>
+        {loading ? <ActivityIndicator size="small" /> : `${nextTime.toLocaleTimeString()} (${nextDuration/1000} sec)`}
       </Text>
       <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Refresh!</Text>
+        <Text style={styles.buttonText} onPress = {loadBusStopData}>Refresh!</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
@@ -33,6 +69,13 @@ const styles = StyleSheet.create({
   },
   arrivalTime: {
     alignItems: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  nextArrivalTime: {
+    alignItems: 'center',
+    fontSize: 12,
     fontWeight: 'bold',
     margin: 10,
   },
